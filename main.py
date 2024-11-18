ec697eb800dcfe05a81011211f43a6a93fb6bd2e
winterbreaker <joejamesdev@hotmail.com>
2024-11-18 06:30:42 -0800

    Update main.py

--- main.py
+++ main.py
@@ -38,6 +38,7 @@ detail_search = False
 PID = 0
 HWND_SEARCH = 0
 HWND_DB = 0
+scraping_finished = False
 
 def get_windows_with_pid_and_hwnd(pid, target_hwnd):
     windows = gw.getWindowsWithTitle('')
@@ -71,7 +72,8 @@ async def run_search_thread(playwright):
     global property_address_for_search
     global PID
     global HWND_SEARCH
-    
+    global scraping_finished
+
     await asyncio.sleep(10)
     browser = await playwright.chromium.launch(
         headless=False, 
@@ -87,40 +89,56 @@ async def run_search_thread(playwright):
     HWND_SEARCH = ctypes.windll.user32.GetForegroundWindow()
     PID = get_pid_from_window_handle(HWND_SEARCH)
 
+    await page.click('article#main-content >> text="Advanced Search"')  # Replace with the correct selector, if needed
+    await page.keyboard.press("End")
+    await asyncio.sleep(2)
+
+    today = datetime.today()
+    one_month_ago = today - relativedelta(months=1)
+
+    await page.fill('#recordedDateRange', str(one_month_ago.strftime(f'%m/%d/%Y')))
+    await page.fill('div.form-field-inline-datepicker > div:last-of-type input', str(today.strftime(f'%m/%d/%Y')))
+    await asyncio.sleep(2)
+
+    for doc_type in doc_types:
+        await page.fill('input#docTypes-input', doc_type['name'])
+        await page.click(f'div.tokenized-nested-select__dropdown label[for="docTypes-item-{str(doc_type['count'])}"]')
+    
+    await page.click('button[type="submit"]')
+    await asyncio.sleep(10)
+
+    result_element = page.locator('p[data-testid="resultsSummary"] > span:first-of-type')
+    result = await result_element.text_content()
+    total = result.split()[2].replace(',', '')
+
+    total_page_number = int(total) / 50 + 1
+    last_page_row_number = int(total) % 50
+
+    search_row_number = 1
+    search_page_number = 1
     while True:
         if not detail_search:
-            await page.click('article#main-content >> text="Advanced Search"')  # Replace with the correct selector, if needed
-            await page.keyboard.press("End")
-            await asyncio.sleep(2)
-
-            today = datetime.today()
-            one_month_ago = today - relativedelta(months=1)
-
-            await page.fill('#recordedDateRange', str(one_month_ago.strftime(f'%m/%d/%Y')))
-            await page.fill('div.form-field-inline-datepicker > div:last-of-type input', str(today.strftime(f'%m/%d/%Y')))
-            await asyncio.sleep(2)
-
-            for doc_type in doc_types:
-                await page.fill('input#docTypes-input', doc_type['name'])
-                await page.click(f'div.tokenized-nested-select__dropdown label[for="docTypes-item-{str(doc_type['count'])}"]')
-            
-            await page.click('button[type="submit"]')
-            await asyncio.sleep(10)
-
             rows = page.locator('table tbody tr')  
-            row_count = await rows.count()
+            address_element = rows.nth(search_row_number).locator('td.col-14 span')
+            await address_element.scroll_into_view_if_needed()
+            address = await address_element.text_content()
+            pieces = re.split(r'[,\s]+', address)
+            property_address_for_search = ' '.join(pieces[:3])
+            search_row_number = search_row_number + 1
 
-            for i in range(row_count):
-                address = await rows.nth(i).locator('td.col-14 span').text_content()
-                if address == 'N/A':
-                    detail_search = False
-                    continue 
-                pieces = re.split(r'[,\s]+', address)
-                if len(pieces) < 3:
-                    detail_search = False
-                    continue 
-                property_address_for_search = ' '.join(pieces[:3])
-                detail_search = True
+            last_row_index = last_page_row_number + 1 if search_page_number == total_page_number else 51
+            if search_row_number == last_row_index:
+                search_row_number = 1
+                await page.click('nav.Pagination > div > button:last-of-type')
+                if search_page_number != total_page_number:
+                    search_page_number = search_page_number + 1
+                else:
+                    scraping_finished = True
+                    break
+            if len(pieces) < 3:
+                continue 
+            
+            detail_search = True
         await asyncio.sleep(1)
 
     await browser.close()
@@ -130,6 +148,7 @@ async def run_db_thread(playwright):
     global property_address_for_search
     global PID
     global HWND_DB
+    global scraping_finished
     
     browser = await playwright.chromium.launch(
         headless=False, 
@@ -182,6 +201,7 @@ async def run_db_thread(playwright):
 
                     # Mailing address
                     mailing_address_full = await owner.nth(17).locator('td:nth-of-type(2)').text_content()
+                    print ("mailing_address: ", mailing_address_full)
                     parts = mailing_address_full.split("<br>")
                     mailing_address = parts[0]
                     mailing_city = parts[-1].split(' ')[0]
@@ -200,8 +220,11 @@ async def run_db_thread(playwright):
                                property_address, property_city, property_state, property_zip]
                     writer.writerow(one_row)
 
-                await page.click('#header_PropertySearch')
-                detail_search = False
+            await page.click('#header_PropertySearch')
+            detail_search = False
+        
+        if scraping_finished:
+            break
         await asyncio.sleep(1)
 
     await browser.close()
@@ -211,14 +234,19 @@ async def run_switch_thread(playwright):
     global PID
     global HWND_DB
     global HWND_SEARCH
+    global scraping_finished
 
     await asyncio.sleep(30)
+    print ("db: ", HWND_DB)
+    print ("search: ", HWND_SEARCH)
 
     while True:
         if detail_search:
             ctypes.windll.user32.SetForegroundWindow(HWND_DB)
         else:
             ctypes.windll.user32.SetForegroundWindow(HWND_SEARCH)
+        if scraping_finished:
+            break
         await asyncio.sleep(1)
 
 async def main():
